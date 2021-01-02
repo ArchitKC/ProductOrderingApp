599598615b9b4a169c243eb198d7e3bfce9b6959
ArchitKC <archithrd@gmail.com>
2021-01-01 21:43:04 -0500

    Adding Pages Content (DB & DTO) and Removing DB

---
+++ ProductOrderingApp/Models/ViewModels/Pages/PagesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ProductOrderingApp.Models.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductOrderingApp.Models.ViewModels.Pages
+{
+    public class PagesViewModel
+    {
+        public PagesViewModel()
+        {
+
+        }
+
+        public PagesViewModel(PageDTO pageData)
+        {
+            Id = pageData.Id;
+            Title = pageData.Title;
+            Slug = pageData.Slug;
+            Body = pageData.Body;
+            Sorting = pageData.Sorting;
+            HasSidebar = pageData.HasSidebar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =5)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 5)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+
+    }
+}
