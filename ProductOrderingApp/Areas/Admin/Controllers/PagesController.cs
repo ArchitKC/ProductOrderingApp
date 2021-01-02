78f922da2125076f04841ceb4e595d8bdaf9e3b2
ArchitKC <archithrd@gmail.com>
2021-01-01 23:04:51 -0500

    Page details and DB edits

--- ProductOrderingApp/Areas/Admin/Controllers/PagesController.cs
+++ ProductOrderingApp/Areas/Admin/Controllers/PagesController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using ProductOrderingApp.Models.Data;
+using ProductOrderingApp.Models.ViewModels.Pages;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ProductOrderingApp.Areas.Admin.Controllers
@@ -11,7 +11,19 @@ namespace ProductOrderingApp.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+
+            List<PagesViewModel> pageLists;
+
+
+            using(ShoppingCart db = new ShoppingCart())
+            {
+                pageLists = db.PagesData.ToArray()
+                    .OrderBy(x=>x.Sorting)
+                    .Select(x=>new PagesViewModel(x))
+                    .ToList();
+            }
+            //Return view with list
+            return View(pageLists);
         }
     }
 }
